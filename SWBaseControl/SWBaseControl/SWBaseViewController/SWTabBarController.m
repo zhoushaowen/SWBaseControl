4b47dd099ee56b410a801712600a8c0e50a59b59
zhoushaowen <348345883@qq.com>
2018-11-13 17:23:31 +0800

    解决ios12.1上tabbar跳动的系统bug

--- SWBaseControl/SWBaseControl/SWBaseViewController/SWTabBarController.m
+++ SWBaseControl/SWBaseControl/SWBaseViewController/SWTabBarController.m
@@ -7,6 +7,53 @@
 //
 
 #import "SWTabBarController.h"
+#import <objc/runtime.h>
+
+@interface SWFixBugTabBarButton : UIView
+
+@end
+
+@implementation SWFixBugTabBarButton
+
++ (void)load {
+    if (@available(iOS 12.1, *)) {
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            Class originalClass = NSClassFromString(@"UITabBarButton");
+            SEL originalSelector = @selector(setFrame:);
+            SEL swizzledSelector = @selector(xp_setFrame:);
+            Method originalMethod = class_getInstanceMethod(originalClass, originalSelector);
+            Method swizzledMethod = class_getInstanceMethod(self, swizzledSelector);
+            //先尝试給源方法添加实现，这里是为了避免源方法没有实现的情况
+            BOOL isSuccess = class_addMethod(originalClass, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
+            if(!isSuccess){//说明源方法实现了
+                class_replaceMethod(originalClass,
+                                    swizzledSelector,
+                                    method_getImplementation(originalMethod),
+                                    method_getTypeEncoding(originalMethod));
+                class_replaceMethod(originalClass,
+                                    originalSelector,
+                                    method_getImplementation(swizzledMethod),
+                                    method_getTypeEncoding(swizzledMethod));
+            }
+        });
+    }
+}
+
+- (void)xp_setFrame:(CGRect)frame {
+    if (!CGRectIsEmpty(self.frame)) {
+        // for iPhone 8/8Plus
+        if (CGRectIsEmpty(frame)) {
+            return;
+        }
+        // for iPhone XS/XS Max/XR
+        frame.size.height = MAX(frame.size.height, 48.0);
+    }
+    [self xp_setFrame:frame];
+}
+
+@end
+
 
 @interface SWTabBarController ()
 
