28e986f1e7e734f8646e596172e8f6eef515ac51
zhoushaowen <348345883@qq.com>
2020-11-17 13:33:30 +0800

    SWDatePickerViewController适配iOS14

--- SWBaseControl.podspec
+++ SWBaseControl.podspec
@@ -2,7 +2,7 @@ Pod::Spec.new do |s|
 
   s.name         = "SWBaseControl"
 
-  s.version      = "1.10.1"
+  s.version      = "1.10.2"
 
   s.homepage      = 'https://github.com/zhoushaowen/SWBaseControl'
 
--- SWBaseControl/SWBaseControl/Other/SWDatePickerViewController.m
+++ SWBaseControl/SWBaseControl/Other/SWDatePickerViewController.m
@@ -9,8 +9,10 @@
 #import "SWDatePickerViewController.h"
 #import <SWCustomPresentation.h>
 
-@interface SWDatePickerViewController ()
+@interface SWDatePickerViewController ()<UIGestureRecognizerDelegate>
 {
+    UIView *_btnBgView;
+    UIView *_pickerContainerView;
     UIDatePicker *_datePicker;
     CALayer *_line;
     UIButton *_cancelBtn,*_confirmBtn;
@@ -39,7 +41,9 @@
     datePickerViewController.datePickerConfig = datePickerConfig;
     datePickerViewController.didSelectedDateBlock = didSelectedDateBlock;
     [self sw_presentCustomModalPresentationWithViewController:datePickerViewController containerViewWillLayoutSubViewsBlock:^(SWPresentationController * _Nonnull presentationController) {
-        presentationController.presentedView.frame = CGRectMake(0, [UIScreen mainScreen].bounds.size.height - 300, [UIScreen mainScreen].bounds.size.width, 300);
+//        CGFloat height = 400;
+//        presentationController.presentedView.frame = CGRectMake(0, [UIScreen mainScreen].bounds.size.height - height, [UIScreen mainScreen].bounds.size.width, height);
+        presentationController.presentedView.frame = presentationController.containerView.bounds;
     } animatedTransitioningModel:nil completion:nil];
     return datePickerViewController;
 }
@@ -54,13 +58,22 @@
 }
 
 - (void)setup {
-    self.view.backgroundColor = [UIColor whiteColor];
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap:)];
+    tap.delegate = self;
+    [self.view addGestureRecognizer:tap];
+    self.view.backgroundColor = [UIColor clearColor];
+    _btnBgView = [UIView new];
+    _btnBgView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:_btnBgView];
+    _pickerContainerView = [UIView new];
+    _pickerContainerView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:_pickerContainerView];
     _line = [CALayer layer];
     _line.backgroundColor = [UIColor colorWithRed:223/255.0 green:223/255.0 blue:223/255.0 alpha:1.0].CGColor;
-    [self.view.layer addSublayer:_line];
+    [_btnBgView.layer addSublayer:_line];
     _datePicker = [[UIDatePicker alloc] init];
     _datePicker.datePickerMode = UIDatePickerModeDate;
-    [self.view addSubview:_datePicker];
+    [_pickerContainerView addSubview:_datePicker];
     NSString *language = [self getLanguage];
     NSArray *titles = nil;
     if([language hasPrefix:@"zh-Hans"]){//简体中文
@@ -73,10 +86,13 @@
         titles = @[@"Cancel",@"OK"];
         [_datePicker setLocale:[NSLocale localeWithLocaleIdentifier:@"en"]];
     }
+    if(self.styleColor == nil){
+        self.styleColor = [UIColor colorWithRed:26/255.0 green:178/255.0 blue:10/255.0 alpha:1.0];
+    }
     NSArray *colors = @[[UIColor colorWithRed:117/255.0f green:117/255.0f blue:117/255.0f alpha:1.0],self.styleColor?:[UIColor colorWithRed:26/255.0 green:178/255.0 blue:10/255.0 alpha:1.0]];
     for(int i=0;i<2;i++){
         UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
-        [self.view addSubview:btn];
+        [_btnBgView addSubview:btn];
         btn.titleLabel.font = [UIFont systemFontOfSize:17];
         [btn setTitle:titles[i] forState:UIControlStateNormal];
         [btn setTitleColor:colors[i] forState:UIControlStateNormal];
@@ -87,10 +103,16 @@
         }
         [btn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
     }
+    if (@available(iOS 13.4, *)) {
+        _datePicker.preferredDatePickerStyle = UIDatePickerStyleWheels;
+    } else {
+        // Fallback on earlier versions
+    }
     if(_datePickerConfig){
         _datePickerConfig(_datePicker);
     }
 }
+
 
 - (NSString*)getPreferredLanguage
 {
@@ -115,10 +137,31 @@
 
 - (void)viewWillLayoutSubviews {
     [super viewWillLayoutSubviews];
-    _line.frame = CGRectMake(0, 40, self.view.bounds.size.width, 1/[UIScreen mainScreen].scale);
-    _datePicker.frame = CGRectMake(0, 40, self.view.bounds.size.width, 300 - 40);
-    _cancelBtn.frame = CGRectMake(0, 0, 156/2.0f, 40);
-    _confirmBtn.frame = CGRectMake(self.view.bounds.size.width - 156/2.0f, 0, 156/2.0f, 40);
+    CGFloat pickerHeight = 390;
+    if (@available(iOS 13.4, *)) {
+        if(_datePicker.datePickerStyle == UIDatePickerStyleWheels){
+            pickerHeight = 330;
+        }
+        else if(_datePicker.datePickerStyle == UIDatePickerStyleCompact){
+            pickerHeight = 60;
+        }
+        else {
+            // Fallback on earlier versions
+        }
+    }
+    CGFloat btnBgViewHeight = 40;
+    CGFloat safeAreaInsetsBottom = 0;
+    if (@available(iOS 11.0, *)) {
+        safeAreaInsetsBottom = self.view.safeAreaInsets.bottom;
+    } else {
+        // Fallback on earlier versions
+    }
+    _btnBgView.frame = CGRectMake(0, self.view.bounds.size.height - pickerHeight - btnBgViewHeight - safeAreaInsetsBottom, self.view.bounds.size.width, btnBgViewHeight);
+    _pickerContainerView.frame = CGRectMake(0, self.view.bounds.size.height - pickerHeight - safeAreaInsetsBottom, self.view.bounds.size.width, pickerHeight + safeAreaInsetsBottom);
+    _datePicker.frame = CGRectMake(0, 0, _pickerContainerView.bounds.size.width, pickerHeight);
+    _line.frame = CGRectMake(0, _btnBgView.bounds.size.height - 1/[UIScreen mainScreen].scale, _btnBgView.bounds.size.width, 1/[UIScreen mainScreen].scale);
+    _cancelBtn.frame = CGRectMake(0, 0, 156/2.0f, btnBgViewHeight);
+    _confirmBtn.frame = CGRectMake(_btnBgView.bounds.size.width - 156/2.0f, 0, 156/2.0f, btnBgViewHeight);
 }
 
 - (void)setStyleColor:(UIColor *)styleColor {
@@ -127,6 +170,7 @@
         _styleColor = [UIColor colorWithRed:26/255.0 green:178/255.0 blue:10/255.0 alpha:1.0];
     }
     [_confirmBtn setTitleColor:_styleColor forState:UIControlStateNormal];
+    _datePicker.tintColor = _styleColor;
 }
 
 - (void)btnClick:(UIButton *)sender {
@@ -139,6 +183,16 @@
             _didSelectedDateBlock(_datePicker.date);
         }
     }
+}
+
+- (void)handleTap:(UITapGestureRecognizer *)ges {
+    [self.view endEditing:YES];
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - UIGestureRecognizerDelegate
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch {
+    return touch.view == gestureRecognizer.view;
 }
 
 - (void)dealloc {
